b9a350ef2319f3ee4929e13bc3eaabc954a8c620
Peliah <pelepoupa@gmail.com>
2025-01-03 00:55:26 +0100

    start game coming up nicely

--- src/components/StartGame.tsx
+++ src/components/StartGame.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const StartGame = () => {
+
+    const arrNumber = [1, 2, 3, 4, 5, 6];
+    const [selectedNumber, setSelectedNumber] = useState(1);
+
+
     return (
         <main>
             <ScoreContainer>
                 <h1>0</h1>
                 <p>Total Score</p>
             </ScoreContainer>
+            <div>
+                {arrNumber.map((value, i) => (
+                    <Box
+                        key={i}
+                        onClick={() => setSelectedNumber(value)}
+                        isSelected={selectedNumber === value}
+                    >
+                        {value}
+                    </Box>
+                ))}
+            </div>
         </main>
     )
 }
@@ -25,4 +42,15 @@ const ScoreContainer = styled.div`
         font-size: 24px;
         font-weight: 500px;
     }
-`
+`;
+
+const Box = styled.div`
+    height: 72px;
+    width: 72px;
+    border: 1px solid black;
+    display: grid;
+    place-items: center;
+    font-size: 24px;
+    background-color: ${(props) => props.isSelected ? 'black' : 'white'};
+    color: ${(props) => props.isSelected ? 'white' : 'black'};
+`;
